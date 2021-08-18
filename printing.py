56a8948563622c208dfd04da52756f2339f90c7c
Meri Engel - Programming Instructor McHenry County College <mengel7@mchenry.edu>
2021-08-18 14:11:50 -0500

    Create printing.py

---
+++ printing.py
@@ -0,0 +1,9 @@
+"""
+    Program 1: printing a song or poem
+"""
+
+print("Mary Had a Little Lamb\n\n")
+print("Mary had a little lamb")
+print("Whose fleece was white as snow")
+print("and every where that Mary went")
+print("the lamb was sure to go.")
